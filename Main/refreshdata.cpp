1a138ac0ecc54fc9904f125432359a888a063c2b
Harish Pradyot 7 <harishpradyot@gmail.com>
2022-04-18 23:33:30 +0530

    Refresh Data Done

--- Main/refreshdata.cpp
+++ Main/refreshdata.cpp
@@ -17,7 +17,7 @@ bool comp(user a, user b)
 	else
 		return false;
 }
-void write_extra_fac(FILE* file, vector<user>::iterator start, vector<user> fac, bool zeroes=false)
+void write_extra_fac(FILE* file, vector<user>::iterator start, vector<user> &fac, bool zeroes=false)
 {
 	fseek(file, -1, SEEK_CUR);
 	if(!zeroes)
@@ -35,7 +35,7 @@ void write_extra_fac(FILE* file, vector<user>::iterator start, vector<user> fac,
 		fputc('\n', file);
 	}
 }
-void write_extra_stu(FILE* file, vector<user>::iterator start, vector<user> stu, int facs)
+void write_extra_stu(FILE* file, vector<user>::iterator start, vector<user> &stu, int facs)
 {
 	fseek(file, -1, SEEK_CUR);
 	for(; start!=stu.end(); start++)
@@ -112,7 +112,65 @@ int refresh_database(string file_name, bool reset=true)
 	}	
 	else
 	{
-		file=fopen(&fname[0], "a+");
+		// Unable to write back at current file pointer position
+		// file=fopen(&fname[0], "a+");
+		// string x="";
+		// FILE* S=fopen("./S.db", "r");
+		// vector<user> stu=get_users(S);
+		// fclose(S);
+
+		// FILE* F=fopen("./F.db", "r");
+		// vector<user> fac=get_users(F);
+		// fclose(F);
+
+		// // Processing first line to get number of faculty already present
+		// char c;
+		// uid_t temp=0;
+		// while((c=fgetc(file)) != '\n')
+		// {
+		// 	if(c==' ')
+		// 	{
+		// 		temp=0;
+		// 		continue;
+		// 	}
+		// 	temp=temp*10+(c-'0');
+		// }
+		// vector<user>::iterator start=upper_bound(fac.begin(), fac.end(), make_pair(temp, x));
+		// if(start->first == temp)
+		// 	start++;
+		// if(start!=fac.end())
+		// 	write_extra_fac(file, start, fac);
+
+		// temp=0;
+		// while(true)
+		// {
+		// 	temp=0;
+		// 	while((c=fgetc(file)) != EOF)
+		// 	{
+		// 		if(c==' ')
+		// 			break;
+		// 		temp=temp*10+(c-'0');
+		// 	}
+		// 	while((c=fgetc(file)) != EOF)
+		// 	{
+		// 		if(c=='\n')
+		// 		{
+		// 			if(start!=fac.end())
+		// 				write_extra_fac(file, start, fac, true);
+		// 			break;
+		// 		}
+		// 	}
+		// 	if(c==EOF)
+		// 		break;
+		// }
+		// start=upper_bound(stu.begin(), stu.end(), make_pair(temp, x));
+		// if(start->first == temp)
+		// 	start++;
+		// write_extra_stu(file, start, stu, fac.size());
+
+		vector<vector<string>> data;
+		file=fopen(&fname[0], "r");
+		
 		string x="";
 		FILE* S=fopen("./S.db", "r");
 		vector<user> stu=get_users(S);
@@ -122,46 +180,69 @@ int refresh_database(string file_name, bool reset=true)
 		vector<user> fac=get_users(F);
 		fclose(F);
 
-		// Processing first line to get number of faculty already present
 		char c;
-		uid_t temp=0;
-		while(c=fgetc(file) != '\n')
+		string temp;
+		vector<string> row;
+		while((c=fgetc(file)) != EOF)
 		{
-			cout<<c<<" this ";
 			if(c==' ')
 			{
-				temp=0;
+				row.push_back(temp);
+				temp.clear();
 				continue;
 			}
-			temp=temp*10+(c-'0');
+			else if(c=='\n')
+			{
+				row.push_back(temp);
+				temp.clear();
+				data.push_back(row);
+				row.clear();
+				continue;
+			}
+			temp=temp+c;
 		}
-		cout<<temp<<endl;
-		vector<user>::iterator start=upper_bound(fac.begin(), fac.end(), make_pair(temp, x));
-		cout<<"Here";
-		cout<<start->first<<"    "<<start->second<<endl;
-		write_extra_fac(file, start, fac);
+		fclose(file);
+		
+		vector<vector<string>>::iterator start=data.begin();
+		vector<vector<string>>::iterator end=data.end()-1;
+		vector<user>::iterator extra=upper_bound(fac.begin(), fac.end(), make_pair((uid_t)stoi(*(start->end()-1)), x));
+		if(extra->first == (uid_t)stoi(*(start->end()-1)))
+			extra++;
 
-		temp=0;
-		while(true)
+		if(extra != fac.end())
 		{
-			temp=0;
-			while(c=fgetc(file) != EOF)
+			for(vector<user>::iterator temp=extra; temp!=fac.end(); temp++)
+				start->push_back(to_string(temp->first));
+			for(vector<vector<string>>::iterator temp=start+1; temp!=data.end(); temp++)
 			{
-				if(c==' ')
-					break;
-				temp=temp*10+(c-'0');
+				for(int i=0; i<fac.end()-extra; i++)
+					temp->push_back("0");
 			}
-			while(c=fgetc(file) != EOF)
-			{
-				if(c=='\n')
-					write_extra_fac(file, start, fac, true);
-			}
-			if(c==EOF)
-				break;
 		}
-		start=upper_bound(stu.begin(), stu.end(), make_pair(temp, x));
-		cout<<start->first<<"    "<<start->second<<endl;
-		write_extra_stu(file, start, stu, fac.size());
+		extra=upper_bound(stu.begin(), stu.end(), make_pair((uid_t)stoi(*(end->begin())), x));
+		if(extra->first == (uid_t)stoi(*(end->begin())))
+			extra++;
+		
+		if(extra != stu.end())
+		{
+			x="0";
+			vector<string> val(fac.size()+1, x);
+			for(vector<user>::iterator temp=extra; temp!=stu.end(); temp++)
+			{
+				val[0]=to_string(extra->first);
+				data.push_back(val);
+			}
+		}
+
+		file=fopen(&fname[0], "w");		
+		start=data.begin();
+		for(;start!=data.end();start++)
+		{
+			for(vector<string>::iterator temp=start->begin(); temp!=start->end(); temp++)
+				fputs(&(*temp+" ")[0], file);
+			fseek(file, -1, SEEK_CUR);
+			fputc('\n', file);
+		}
 	}	
 	fclose(file);
 }
